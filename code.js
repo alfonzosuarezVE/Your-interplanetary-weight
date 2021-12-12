622e6874eef6ca59815fa8ab8ba27aaec3089623
Alfonzo Suarez <samuelalfonzo2@hotmail.com>
2021-12-12 14:10:30 -0430

    Agregamos texto al resultado.

--- code.js
+++ code.js
@@ -1,8 +1,9 @@
 var usuario = prompt("Cual es tu peso");
+var peso = usuario;
 var g_tierra = 9.8;
 var g_marte = 3.6;
 var g_jupiter = 24.8;
-var peso = usuario;
+
 var peso_final;
 
 
@@ -10,4 +11,5 @@ peso_final = peso * g_marte / g_tierra;
 
 
 
-document.write(peso_final.toFixed(2));
+
+document.write("Tu peso es " + peso_final.toFixed(2) + " kilogramos");
