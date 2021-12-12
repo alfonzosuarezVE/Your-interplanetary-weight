d1c6afb00d219aa6b33cbd5376f76dfc417c7031
Alfonzo Suarez <samuelalfonzo2@hotmail.com>
2021-12-12 14:48:19 -0430

    Se agrego condicionales al codigo

--- code.js
+++ code.js
@@ -1,5 +1,4 @@
-var usuario = prompt("Cual es tu peso");
-var peso = usuario;
+var usuario = parseInt(prompt("Cual es tu peso en KG"));
 var g_tierra = 9.8;
 var g_marte = 3.6;
 var g_jupiter = 24.8;
@@ -7,9 +6,26 @@ var g_jupiter = 24.8;
 var peso_final;
 
 
-peso_final = peso * g_marte / g_tierra;
+
+
+var planeta = parseInt(prompt("Elige tu planeta. 1 es Marte. 2 es Jupiter"));
+
+if (planeta == 1)
+{
+    peso_final = usuario * g_marte / g_tierra;
+    document.write("Tu peso en Marte es " + peso_final.toFixed(2) + " kilogramos");
+}
+else if (planeta == 2)
+{
+    peso_final = usuario * g_jupiter / g_tierra;
+    document.write("Tu peso en Jupiter es " + peso_final.toFixed(2) + " kilogramos");
+}
+else 
+{
+    document.write("Eso no es un numero!")
+}
 
 
 
 
-document.write("Tu peso es " + peso_final.toFixed(2) + " kilogramos");
+
